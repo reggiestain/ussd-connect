944520a83ffaaa45889eadf8925e8f7e5d8191e2
reggiestain <reggiestain@gmail.com>
2019-05-04 15:24:57 +0200

    Initial commit

---
+++ database/migrations/2019_05_04_125329_create_sessions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sessionid',100);
+            $table->string('fullname',100);
+            $table->string('phone',11)->unique();
+            $table->string('mno',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
